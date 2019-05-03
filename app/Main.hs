c321947f3f173b3bd8584b8e758d0bbec5c3c978
Aearnus <tslimkemann42@gmail.com>
2019-05-03 04:08:37 -0700

    beams and stress

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import Softbody
 import qualified Data.Array.Accelerate as A
 import Data.Array.Accelerate.LLVM.Native as CPU
-import Data.Array.Accelerate.Data.Complex
+import Data.Complex
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
 import Graphics.Gloss.Data.Color
@@ -16,11 +16,18 @@ update_v c v t d = hs_new_v c v (A.fromList A.Z $ [t :+ 0]) (A.fromList A.Z $ [d
 ---
 
 display_v :: (V, C) -> Picture
-display_v (v, c) = nodePictures
+display_v (v, c) = pictures [nodePictures, edgePictures]
   where
-    rectangleAt x y = translate x y $ rectangleSolid 10 10
     complexToFloatTuple (real :+ imag) = (realToFrac real, realToFrac imag)
-    nodePictures = pictures (map (\(pi, delta, alpha, rho) -> uncurry rectangleAt $ complexToFloatTuple pi) (A.toList v))
+    nodeList = A.toList v
+
+    rectangleAt x y = translate x y $ rectangleSolid 10 10
+    nodePictures = pictures (map (\(pi, delta, alpha, rho) -> uncurry rectangleAt $ complexToFloatTuple pi) nodeList)
+
+    nodePairsWithDistance = zip [(pi1, pi2) | (pi1, _, _, _) <- nodeList, (pi2, _, _, _) <- nodeList] (A.toList c)
+    edgeColor n = mixColors 10 n black red
+    edgePicture ((pi1, pi2), d) = color (edgeColor . realToFrac . abs $ (magnitude $ pi2 - pi1) - d) (line $ complexToFloatTuple <$> [pi1, pi2])
+    edgePictures = pictures (edgePicture <$> nodePairsWithDistance) 
 
 tick_v :: ViewPort -> Float -> (V, C) -> (V, C)
 tick_v _ t (v, c) = (update_v c v (realToFrac t * 8) 1, c)
